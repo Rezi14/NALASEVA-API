08ce326cbb544b913e00c6b955d3cf8e6f720994
Rezikecap <fahrezianggalesmana14@gmail.com>
2026-05-28 19:45:43 +0700

    NALASEVA API

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
-class KlinikSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     public function run(): void
     {
