b3ae445c837b85e8f02131bee244ec1b06601b39
Rezikecap <fahrezianggalesmana14@gmail.com>
2026-05-28 19:42:16 +0700

    NALASEVA API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            KlinikSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
