5c44aa1d2ef5411af17b607aaaeee23913c87978
Rezikecap <fahrezianggalesmana14@gmail.com>
2026-06-03 03:08:48 +0700

    NALASEVA API

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -17,11 +17,11 @@ class AdminSeeder extends Seeder
         \App\Models\User::updateOrCreate(
             ['email' => 'admin@admin.com'],
             [
-                'name' => 'Admin Utama Puskesmas', 
+                'name' => 'Admin Utama NalaSeva', 
                 'password' => $password, 
                 'role' => 'admin', 
                 'phone' => '081234567890',
-                'address' => 'Jl. Kesehatan No. 1, Puskesmas Admin',
+                'address' => 'Jl. Kesehatan No. 1, Puskesmas NalaSeva',
                 'national_id' => '1234567890123456',
                 'gender' => 'Laki-laki',
                 'birth_date' => '1990-01-01',
@@ -31,14 +31,56 @@ class AdminSeeder extends Seeder
         \App\Models\User::updateOrCreate(
             ['email' => 'admin2@admin.com'],
             [
-                'name' => 'Admin Asisten NalaSeva', 
+                'name' => 'Admin Kedua NalaSeva', 
                 'password' => $password, 
                 'role' => 'admin', 
                 'phone' => '081234567891',
-                'address' => 'Jl. Sehat Walafiat No. 2, Puskesmas Admin',
+                'address' => 'Jl. Sehat Walafiat No. 2, Puskesmas NalaSeva',
                 'national_id' => '1234567890123457',
                 'gender' => 'Perempuan',
                 'birth_date' => '1993-08-20',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin3@admin.com'],
+            [
+                'name' => 'Admin Ketiga NalaSeva', 
+                'password' => $password, 
+                'role' => 'admin', 
+                'phone' => '081234567892',
+                'address' => 'Jl. Bhakti Husada No. 3, Puskesmas NalaSeva',
+                'national_id' => '1234567890123458',
+                'gender' => 'Laki-laki',
+                'birth_date' => '1991-04-12',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin4@admin.com'],
+            [
+                'name' => 'Admin Keempat NalaSeva', 
+                'password' => $password, 
+                'role' => 'admin', 
+                'phone' => '081234567893',
+                'address' => 'Jl. Kebugaran No. 4, Puskesmas NalaSeva',
+                'national_id' => '1234567890123459',
+                'gender' => 'Perempuan',
+                'birth_date' => '1994-09-05',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin5@admin.com'],
+            [
+                'name' => 'Admin Kelima NalaSeva', 
+                'password' => $password, 
+                'role' => 'admin', 
+                'phone' => '081234567894',
+                'address' => 'Jl. Sanitasi No. 5, Puskesmas NalaSeva',
+                'national_id' => '1234567890123460',
+                'gender' => 'Laki-laki',
+                'birth_date' => '1988-12-30',
             ]
         );
     }
--- database/seeders/PharmacistSeeder.php
+++ database/seeders/PharmacistSeeder.php
@@ -21,7 +21,7 @@ class PharmacistSeeder extends Seeder
                 'password' => $password, 
                 'role' => 'pharmacist', 
                 'phone' => '089876543210',
-                'address' => 'Jl. Farmasi No. 1, Puskesmas Apotek',
+                'address' => 'Jl. Farmasi No. 1, Apotek NalaSeva',
                 'national_id' => '9876543210987654',
                 'gender' => 'Perempuan',
                 'birth_date' => '1992-05-15',
@@ -31,14 +31,56 @@ class PharmacistSeeder extends Seeder
         \App\Models\User::updateOrCreate(
             ['email' => 'apoteker2@apoteker.com'],
             [
-                'name' => 'Apoteker Asisten NalaSeva', 
+                'name' => 'Apoteker Kedua NalaSeva', 
                 'password' => $password, 
                 'role' => 'pharmacist', 
                 'phone' => '089876543211',
-                'address' => 'Jl. Resep Obat No. 2, Puskesmas Apotek',
+                'address' => 'Jl. Resep Obat No. 2, Apotek NalaSeva',
                 'national_id' => '9876543210987655',
                 'gender' => 'Laki-laki',
                 'birth_date' => '1995-11-10',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'apoteker3@apoteker.com'],
+            [
+                'name' => 'Apoteker Ketiga NalaSeva', 
+                'password' => $password, 
+                'role' => 'pharmacist', 
+                'phone' => '089876543212',
+                'address' => 'Jl. Obat Manjur No. 3, Apotek NalaSeva',
+                'national_id' => '9876543210987656',
+                'gender' => 'Perempuan',
+                'birth_date' => '1993-02-18',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'apoteker4@apoteker.com'],
+            [
+                'name' => 'Apoteker Keempat NalaSeva', 
+                'password' => $password, 
+                'role' => 'pharmacist', 
+                'phone' => '089876543213',
+                'address' => 'Jl. Kapsul Sehat No. 4, Apotek NalaSeva',
+                'national_id' => '9876543210987657',
+                'gender' => 'Laki-laki',
+                'birth_date' => '1996-07-25',
+            ]
+        );
+
+        \App\Models\User::updateOrCreate(
+            ['email' => 'apoteker5@apoteker.com'],
+            [
+                'name' => 'Apoteker Kelima NalaSeva', 
+                'password' => $password, 
+                'role' => 'pharmacist', 
+                'phone' => '089876543214',
+                'address' => 'Jl. Racikan No. 5, Apotek NalaSeva',
+                'national_id' => '9876543210987658',
+                'gender' => 'Perempuan',
+                'birth_date' => '1994-10-02',
             ]
         );
     }
